3d447a716933511abf63754895032a0066d4f0c9
Aryan Kushwaha <59092136+funinkina@users.noreply.github.com>
2025-04-06 10:25:46 +0530

    fix: ensure PORT environment variable is consistently set in main.py

--- api/main.py
+++ api/main.py
@@ -1,4 +1,8 @@
 import uvicorn
+import os
+
+port = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
